9717d406fa971baff9dc3431e20b7b849752c4f0
Shvam15 <shivam.sngh01@gmail.com>
2024-10-06 12:59:54 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import cors from 'cors'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import path from "path"
+import {fileURLToPath} from "url"
 
 // import Razorpay from 'razorpay';
 
@@ -15,6 +16,9 @@ dotenv.config()
 
 //database config
 connectDB()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 //rest object
 const app = express();
